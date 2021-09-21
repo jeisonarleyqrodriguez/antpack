4b797f91552f48d4ed2c4a7addc8ff65a2a3cd20
Jeison Quiñones <jeisonarleyqrodriguez@gmail.com>
2021-09-21 00:27:24 -0500

    obtener todos los usuarios y listarlos, se crea servicio para conectar con la API, se crea componentes para jugadores, se crea el navbar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {MatSnackBar} from '@angular/material/snack-bar';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'antpack';
+  constructor(private _snackBar: MatSnackBar) {}
+
+  showMessage():void {
+    this._snackBar.open("hola soy Jeison", "cerrar", {
+      duration: 3000
+    })
+  }
 }
